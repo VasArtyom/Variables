6c618a0577d4675a67b20d626c2332ac830ce8fe
Vasliaev Artyom <arcryt2122@gmail.com>
2024-11-21 00:37:30 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,5 @@
+DoneHomeWork=12
+Number_of_hours_spent = 1.5
+CourseName = 'Python'
+TimeForOneTask = Number_of_hours_spent / DoneHomeWork
+print(CourseName, ", всего задач: ", DoneHomeWork, ", затрачено часов: ", Number_of_hours_spent, "среднее время выполнения ", TimeForOneTask, " часов")
